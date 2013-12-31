d05c2ddbca24e16bcadf1693f13f76d4426638db
Heinrich Hartmann <hartmann@uni-koblenz.de>
2013-12-31 12:08:49 +0100

    Completed basic version

---
+++ test/test_argparse.py
@@ -0,0 +1,57 @@
+import unittest
+
+# use symlink in test directory
+from zmqdump import *
+
+class TestArgParse(unittest.TestCase):
+
+    parser = None
+
+    def setUp(self):
+        zdp = Zmqdump()
+        self.parser = zdp.parser
+
+    def tearDown(self):
+        pass
+
+    def testArgs(self):
+        def pArgStr(s): 
+            return self.parser.parse_args(s.split())
+
+        ns = pArgStr("SUB tcp://127.0.0.1:8000")
+        self.assertEqual(ns.socket_type, "SUB")
+        self.assertEqual(ns.endpoint, "tcp://127.0.0.1:8000")
+        self.assertEqual(ns.hwm, 1000)
+        self.assertEqual(ns.bind, False)
+        self.assertEqual(ns.delay, 0)
+        
+        ns = pArgStr("PUB tcp://127.0.0.1:8000 -d 200 -b")
+        self.assertEqual(ns.socket_type, "PUB")
+        self.assertEqual(ns.endpoint, "tcp://127.0.0.1:8000")
+        self.assertEqual(ns.hwm, 1000)
+        self.assertEqual(ns.bind, True)
+        self.assertEqual(ns.delay, 200)
+        
+        ns = pArgStr("PULL tcp://127.0.0.1:8000")
+        self.assertEqual(ns.socket_type, "PULL")
+        self.assertEqual(ns.endpoint, "tcp://127.0.0.1:8000")
+        self.assertEqual(ns.hwm, 1000)
+        self.assertEqual(ns.bind, False)
+        self.assertEqual(ns.delay, 0)
+
+        ns = pArgStr("PULL tcp://127.0.0.1:8000 -hwm 1")
+        self.assertEqual(ns.socket_type, "PULL")
+        self.assertEqual(ns.endpoint, "tcp://127.0.0.1:8000")
+        self.assertEqual(ns.hwm, 1)
+        self.assertEqual(ns.bind, False)
+        self.assertEqual(ns.delay, 0)
+
+        ns = pArgStr("--bind PUSH -hwm 1 tcp://127.0.0.1:8000 -d 100 ")
+        self.assertEqual(ns.socket_type, "PUSH")
+        self.assertEqual(ns.endpoint, "tcp://127.0.0.1:8000")
+        self.assertEqual(ns.hwm, 1)
+        self.assertEqual(ns.bind, True)
+        self.assertEqual(ns.delay,  100)
+
+if __name__ == '__main__':
+    unittest.main()
